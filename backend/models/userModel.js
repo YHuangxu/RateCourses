02e093263a7c561d58ef23017876a9fdde006e92
MengBanana <42224358+MengBanana@users.noreply.github.com>
2019-02-21 15:24:50 -0800

    Add files via upload

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,68 @@
-var mongoose = require('mongoose');
+const Joi = require('joi');
+const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
-  username: String,
-  firstname: String,
-  lastname: String,
-  password: String,
-  email: String,
-  bio: String,
-  image: String,
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 15
+    },
+
+    firstname: {
+        type: String,
+    },
+    lastname: String,
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    bio: String,
+    image: String,
 }, {timestamps: true});
 
-var userModel = mongoose.model('userModel', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+const userModel = mongoose.model('userModel', userSchema);
 
 module.exports = userModel;
+exports.validate = validateUser;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
